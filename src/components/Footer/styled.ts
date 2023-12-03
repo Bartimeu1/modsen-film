e5a1a64450cd3107964d48215a9091c924f86578
Ivan Sapego <sapego-ivan@list.ru>
2023-12-03 14:10:34 +0300

    feat: adaptive footer

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -14,6 +14,14 @@ export const StyledFooter = styled.footer`
     width: 100%;
     height: 1px;
   }
+
+  @media (max-width: 830px) {
+    padding: 30px 0 8px 0;
+  }
+
+  @media (max-width: 480px) {
+    padding-top: 10px;
+  }
 `;
 
 export const FooterContent = styled.div`
@@ -21,10 +29,24 @@ export const FooterContent = styled.div`
   justify-content: space-between;
   align-items: flex-start;
   margin-bottom: 45px;
+
+  @media (max-width: 650px) {
+    flex-direction: column;
+    margin-bottom: 38px;
+  }
 `;
 
 export const FooterInfo = styled.div`
   display: flex;
+
+  @media (max-width: 650px) {
+    margin-bottom: 20px;
+  }
+
+  @media (max-width: 480px) {
+    flex-direction: column;
+    margin-bottom: 40px;
+  }
 `;
 
 export const FooterInfoText = styled.p`
@@ -37,12 +59,31 @@ export const FooterInfoText = styled.p`
   &:last-child {
     margin-right: 0;
   }
+
+  @media (max-width: 830px) {
+    font-size: ${(props) => props.theme.fontSize.sm};
+    margin-right: 10px;
+    max-width: 230px;
+  }
+
+  @media (max-width: 480px) {
+    margin: 0 0 20px 0;
+    max-width: 180px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
 `;
 
 export const FooterSocial = styled.div`
   display: flex;
   align-items: center;
   margin: 0 -13px;
+
+  @media (max-width: 480px) {
+    align-self: center;
+  }
 `;
 
 export const FooterSocialLink = styled.a`
@@ -55,4 +96,12 @@ export const FooterLabel = styled.h2`
   text-align: center;
   font-weight: 500;
   opacity: 40%;
+
+  @media (max-width: 650px) {
+    font-size: 15px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 12px;
+  }
 `;
